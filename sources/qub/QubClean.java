15c30593566aacbb74b68c663c8c107974f4fcc2
Dan Schulte <dan.schulte.qub@live.com>
2021-06-29 12:27:33 -0700

    Update dependencies and fix tests

--- sources/qub/QubClean.java
+++ sources/qub/QubClean.java
@@ -14,11 +14,85 @@ public interface QubClean
     {
         PreCondition.assertNotNull(process, "process");
 
-        process.createCommandLineActions()
+        final CommandLineActions actions = process.createCommandLineActions()
             .setApplicationName(QubClean.applicationName)
-            .setApplicationDescription(QubClean.applicationDescription)
-            .addAction(QubCleanRun::addAction)
-            .addAction(CommandLineLogsAction::addAction)
-            .run();
+            .setApplicationDescription(QubClean.applicationDescription);
+
+        actions.addAction("run", QubClean::runAction)
+            .setDescription("Clean build outputs from source code projects.")
+            .setDefaultAction();
+        actions.addAction(CommandLineLogsAction::addAction);
+
+        actions.run();
+    }
+
+    static void runAction(DesktopProcess process, CommandLineAction action)
+    {
+        PreCondition.assertNotNull(process, "process");
+        PreCondition.assertNotNull(action, "action");
+
+        final CommandLineParameters parameters = action.createCommandLineParameters(process);
+        final CommandLineParameter<Folder> folderToCleanParameter = parameters.addPositionalFolder("folder", process)
+            .setValueName("<folder-to-clean>")
+            .setDescription("The folder to clean. Defaults to the current folder.");
+        final CommandLineParameterVerbose verboseParameter = parameters.addVerbose(process);
+        final CommandLineParameterProfiler profiler = parameters.addProfiler(process, QubClean.class);
+        final CommandLineParameterHelp help = parameters.addHelp();
+
+        if (!help.showApplicationHelpLines(process).await())
+        {
+            profiler.await();
+
+            final Folder folderToClean = folderToCleanParameter.getValue().await();
+            final Folder qubCleanDataFolder = process.getQubProjectDataFolder().await();
+
+            final LogStreams logStreams = CommandLineLogsAction.getLogStreamsFromDataFolder(qubCleanDataFolder, process.getOutputWriteStream(), verboseParameter.getVerboseCharacterToByteWriteStream().await());
+            try (final Disposable logStream = logStreams.getLogStream())
+            {
+                final IndentedCharacterWriteStream output = IndentedCharacterWriteStream.create(logStreams.getOutput());
+                final VerboseCharacterToByteWriteStream verbose = logStreams.getVerbose();
+
+                output.writeLine("Cleaning...").await();
+
+                if (!folderToClean.exists().await())
+                {
+                    output.writeLine("The folder " + folderToClean + " doesn't exist.").await();
+                }
+                else
+                {
+                    int foldersCleaned = 0;
+                    for (final String folderNameToClean : Iterable.create("outputs", "out", "target", "output", "dist"))
+                    {
+                        final Folder folderToDelete = folderToClean.getFolder(folderNameToClean).await();
+                        verbose.writeLine("Checking if " + folderToDelete + " exists...").await();
+                        if (!folderToDelete.exists().await())
+                        {
+                            verbose.writeLine("Doesn't exist.").await();
+                        }
+                        else
+                        {
+                            ++foldersCleaned;
+                            output.write("Deleting folder " + folderToDelete + "...").await();
+                            folderToDelete.delete()
+                                .then(() ->
+                                {
+                                    output.writeLine(" Done.").await();
+                                })
+                                .catchError((Throwable error) ->
+                                {
+                                    output.writeLine(" Failed.").await();
+                                    output.indent(() -> output.writeLine(error.getMessage()).await());
+                                })
+                                .await();
+                        }
+                    }
+
+                    if (foldersCleaned == 0)
+                    {
+                        output.writeLine("Found no folders to delete.").await();
+                    }
+                }
+            }
+        }
     }
 }
--- tests/qub/QubCleanTests.java
+++ tests/qub/QubCleanTests.java
@@ -15,7 +15,7 @@ public interface QubCleanTests
                 });
             });
             
-            runner.testGroup("run(QubProcess)", () ->
+            runner.testGroup("run(DesktopProcess)", () ->
             {
                 runner.test("with null", (Test test) ->
                 {
@@ -23,58 +23,218 @@ public interface QubCleanTests
                         new PreConditionFailure("process cannot be null."));
                 });
 
-                runner.test("with -?", (Test test) ->
+                runner.test("with -?",
+                    (TestResources resources) -> Tuple.create(resources.createFakeDesktopProcess("-?")),
+                    (Test test, FakeDesktopProcess process) ->
                 {
-                    try (final FakeDesktopProcess process = FakeDesktopProcess.create("-?"))
-                    {
-                        QubClean.run(process);
+                    QubClean.run(process);
 
-                        test.assertEqual(
-                            Iterable.create(
-                                "Usage: qub-clean [--action=]<action-name> [--help]",
-                                "  Used to clean build outputs from source code projects.",
-                                "  --action(a): The name of the action to invoke.",
-                                "  --help(?):   Show the help message for this application.",
-                                "",
-                                "Actions:",
-                                "  logs:          Show the logs folder.",
-                                "  run (default): Clean build outputs from source code projects."),
-                            Strings.getLines(process.getOutputWriteStream().getText().await()));
-                    }
+                    test.assertLinesEqual(
+                        Iterable.create(
+                            "Usage: qub-clean [--action=]<action-name> [--help]",
+                            "  Used to clean build outputs from source code projects.",
+                            "  --action(a): The name of the action to invoke.",
+                            "  --help(?):   Show the help message for this application.",
+                            "",
+                            "Actions:",
+                            "  logs:          Show the logs folder.",
+                            "  run (default): Clean build outputs from source code projects."),
+                        process.getOutputWriteStream());
+                    test.assertLinesEqual(
+                        Iterable.create(),
+                        process.getErrorWriteStream());
+                    test.assertEqual(-1, process.getExitCode());
                 });
 
-                runner.test("with run -?", (Test test) ->
+                runner.test("with run -?",
+                    (TestResources resources) -> Tuple.create(resources.createFakeDesktopProcess("run", "-?")),
+                    (Test test, FakeDesktopProcess process) ->
                 {
-                    try (final FakeDesktopProcess process = FakeDesktopProcess.create("run", "-?"))
-                    {
-                        QubClean.run(process);
+                    QubClean.run(process);
 
-                        test.assertEqual(
-                            Iterable.create(
-                                "Usage: qub-clean run [[--folder=]<folder-to-clean>] [--verbose] [--profiler] [--help]",
-                                "  Clean build outputs from source code projects.",
-                                "  --folder:     The folder to clean. Defaults to the current folder.",
-                                "  --verbose(v): Whether or not to show verbose logs.",
-                                "  --profiler:   Whether or not this application should pause before it is run to allow a profiler to be attached.",
-                                "  --help(?):    Show the help message for this application."),
-                            Strings.getLines(process.getOutputWriteStream().getText().await()));
-                    }
+                    test.assertLinesEqual(
+                        Iterable.create(
+                            "Usage: qub-clean run [[--folder=]<folder-to-clean>] [--verbose] [--profiler] [--help]",
+                            "  Clean build outputs from source code projects.",
+                            "  --folder:     The folder to clean. Defaults to the current folder.",
+                            "  --verbose(v): Whether or not to show verbose logs.",
+                            "  --profiler:   Whether or not this application should pause before it is run to allow a profiler to be attached.",
+                            "  --help(?):    Show the help message for this application."),
+                        process.getOutputWriteStream());
+                    test.assertLinesEqual(
+                        Iterable.create(),
+                        process.getErrorWriteStream());
+                    test.assertEqual(-1, process.getExitCode());
                 });
 
-                runner.test("with logs -?", (Test test) ->
+                runner.test("with logs -?",
+                    (TestResources resources) -> Tuple.create(resources.createFakeDesktopProcess("logs", "-?")),
+                    (Test test, FakeDesktopProcess process) ->
                 {
-                    try (final FakeDesktopProcess process = FakeDesktopProcess.create("logs", "-?"))
-                    {
-                        QubClean.run(process);
+                    QubClean.run(process);
 
-                        test.assertEqual(
-                            Iterable.create(
-                                "Usage: qub-clean logs [--openWith] [--help]",
-                                "  Show the logs folder.",
-                                "  --openWith: The application to use to open the logs folder.",
-                                "  --help(?):  Show the help message for this application."),
-                            Strings.getLines(process.getOutputWriteStream().getText().await()));
-                    }
+                    test.assertLinesEqual(
+                        Iterable.create(
+                            "Usage: qub-clean logs [--openWith] [--help]",
+                            "  Show the logs folder.",
+                            "  --openWith: The application to use to open the logs folder.",
+                            "  --help(?):  Show the help message for this application."),
+                        process.getOutputWriteStream());
+                    test.assertLinesEqual(
+                        Iterable.create(),
+                        process.getErrorWriteStream());
+                    test.assertEqual(-1, process.getExitCode());
+                });
+
+                runner.test("when folder to clean doesn't exist",
+                    (TestResources resources) -> Tuple.create(resources.createFakeDesktopProcess("/folder/to/clean/")),
+                    (Test test, FakeDesktopProcess process) ->
+                {
+                    QubClean.run(process);
+
+                    test.assertLinesEqual(
+                        Iterable.create(
+                            "Cleaning...",
+                            "The folder /folder/to/clean/ doesn't exist."),
+                        process.getOutputWriteStream());
+                    test.assertLinesEqual(
+                        Iterable.create(),
+                        process.getErrorWriteStream());
+                    test.assertEqual(0, process.getExitCode());
+                    test.assertTrue(process.getQubProjectDataFolder().await().exists().await());
+                });
+
+                runner.test("when no folders to clean exist",
+                    (TestResources resources) -> Tuple.create(resources.createFakeDesktopProcess("/folder/to/clean/")),
+                    (Test test, FakeDesktopProcess process) ->
+                {
+                    process.getFileSystem().createFolder("/folder/to/clean/").await();
+
+                    QubClean.run(process);
+
+                    test.assertLinesEqual(
+                        Iterable.create(
+                            "Cleaning...",
+                            "Found no folders to delete."),
+                        process.getOutputWriteStream());
+                    test.assertLinesEqual(
+                        Iterable.create(),
+                        process.getErrorWriteStream());
+                    test.assertEqual(0, process.getExitCode());
+                    test.assertTrue(process.getQubProjectDataFolder().await().exists().await());
+                });
+
+                runner.test("when no folders to clean exist with verbose",
+                    (TestResources resources) -> Tuple.create(resources.createFakeDesktopProcess("/folder/to/clean/", "--verbose")),
+                    (Test test, FakeDesktopProcess process) ->
+                {
+                    process.getFileSystem().createFolder("/folder/to/clean/").await();
+
+                    QubClean.run(process);
+
+                    test.assertLinesEqual(
+                        Iterable.create(
+                            "Cleaning...",
+                            "VERBOSE: Checking if /folder/to/clean/outputs/ exists...",
+                            "VERBOSE: Doesn't exist.",
+                            "VERBOSE: Checking if /folder/to/clean/out/ exists...",
+                            "VERBOSE: Doesn't exist.",
+                            "VERBOSE: Checking if /folder/to/clean/target/ exists...",
+                            "VERBOSE: Doesn't exist.",
+                            "VERBOSE: Checking if /folder/to/clean/output/ exists...",
+                            "VERBOSE: Doesn't exist.",
+                            "VERBOSE: Checking if /folder/to/clean/dist/ exists...",
+                            "VERBOSE: Doesn't exist.",
+                            "Found no folders to delete."),
+                        process.getOutputWriteStream());
+                    test.assertLinesEqual(
+                        Iterable.create(),
+                        process.getErrorWriteStream());
+                    test.assertEqual(0, process.getExitCode());
+                    test.assertTrue(process.getQubProjectDataFolder().await().exists().await());
+                });
+
+                runner.test("when outputs folder exists",
+                    (TestResources resources) -> Tuple.create(resources.createFakeDesktopProcess("/folder/to/clean/")),
+                    (Test test, FakeDesktopProcess process) ->
+                {
+                    final Folder folderToClean = process.getFileSystem().getFolder("/folder/to/clean/").await();
+                    final Folder outputsFolder = folderToClean.getFolder("outputs").await();
+                    outputsFolder.create().await();
+
+                    QubClean.run(process);
+
+                    test.assertLinesEqual(
+                        Iterable.create(
+                            "Cleaning...",
+                            "Deleting folder " + outputsFolder + "... Done."),
+                        process.getOutputWriteStream());
+                    test.assertLinesEqual(
+                        Iterable.create(),
+                        process.getErrorWriteStream());
+                    test.assertEqual(0, process.getExitCode());
+                    test.assertFalse(outputsFolder.exists().await());
+                    test.assertTrue(folderToClean.exists().await());
+                    test.assertTrue(process.getQubProjectDataFolder().await().exists().await());
+                });
+
+                runner.test("when outputs folder exists with verbose",
+                    (TestResources resources) -> Tuple.create(resources.createFakeDesktopProcess("/folder/to/clean/", "--verbose")),
+                    (Test test, FakeDesktopProcess process) ->
+                {
+                    final Folder folderToClean = process.getFileSystem().getFolder("/folder/to/clean/").await();
+                    final Folder outputsFolder = folderToClean.getFolder("outputs").await();
+                    outputsFolder.create().await();
+
+                    QubClean.run(process);
+
+                    test.assertLinesEqual(
+                        Iterable.create(
+                            "Cleaning...",
+                            "VERBOSE: Checking if /folder/to/clean/outputs/ exists...",
+                            "Deleting folder /folder/to/clean/outputs/... Done.",
+                            "VERBOSE: Checking if /folder/to/clean/out/ exists...",
+                            "VERBOSE: Doesn't exist.",
+                            "VERBOSE: Checking if /folder/to/clean/target/ exists...",
+                            "VERBOSE: Doesn't exist.",
+                            "VERBOSE: Checking if /folder/to/clean/output/ exists...",
+                            "VERBOSE: Doesn't exist.",
+                            "VERBOSE: Checking if /folder/to/clean/dist/ exists...",
+                            "VERBOSE: Doesn't exist."),
+                        process.getOutputWriteStream());
+                    test.assertLinesEqual(
+                        Iterable.create(),
+                        process.getErrorWriteStream());
+                    test.assertEqual(0, process.getExitCode());
+                    test.assertFalse(outputsFolder.exists().await());
+                    test.assertTrue(folderToClean.exists().await());
+                    test.assertTrue(process.getQubProjectDataFolder().await().exists().await());
+                });
+
+                runner.test("when outputs folder exists but can't be deleted",
+                    (TestResources resources) -> Tuple.create(resources.createFakeDesktopProcess("/folder/to/clean/")),
+                    (Test test, FakeDesktopProcess process) ->
+                {
+                    final InMemoryFileSystem fileSystem = process.getFileSystem();
+                    final Folder folderToClean = fileSystem.getFolder("/folder/to/clean/").await();
+                    final Folder outputsFolder = folderToClean.getFolder("outputs").await();
+                    outputsFolder.create().await();
+                    fileSystem.setFolderCanDelete(outputsFolder.getPath(), false);
+
+                    QubClean.run(process);
+
+                    test.assertLinesEqual(
+                        Iterable.create(
+                            "Cleaning...",
+                            "Deleting folder /folder/to/clean/outputs/... Failed.",
+                            "  The folder at \"/folder/to/clean/outputs/\" doesn't exist."),
+                        process.getOutputWriteStream());
+                    test.assertLinesEqual(
+                        Iterable.create(),
+                        process.getErrorWriteStream());
+                    test.assertEqual(0, process.getExitCode());
+                    test.assertTrue(outputsFolder.exists().await());
+                    test.assertTrue(process.getQubProjectDataFolder().await().exists().await());
                 });
             });
         });
